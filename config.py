d6632b20ba13437d46e5b308998aa11f16a9a953
bensonlag56 <bensonlag200@gmai.com>
2025-06-16 15:25:08 -0500

    Correccion de la base de datos

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-DB_HOST = os.getenv('mysql.railway.internal')
-DB_USER = os.getenv('root')
-DB_PASSWORD = os.getenv('JqsHAilfOvOfsxBJSzyKjQnWOTshsNAz')
-DB_NAME = os.getenv('railway')
-DB_PORT = int(os.getenv('3306', 3306))
+DB_HOST = os.getenv('MYSQLHOST')
+DB_USER = os.getenv('MYSQLUSER')
+DB_PASSWORD = os.getenv('MYSQLPASSWORD')
+DB_NAME = os.getenv('MYSQLDATABASE')
+DB_PORT = int(os.getenv('MYSQLPORT',  3306))
 
 # Rutas de carpetas
 UPLOAD_FOLDER = 'imagenes'
